b169b893c00666a100c76d7f12443b4a051ca168
Satyam <100795600+RaoSam-Code@users.noreply.github.com>
2026-08-30 22:54:00 +0530

    date

--- lib/calendar-service.ts
+++ lib/calendar-service.ts
@@ -12,6 +12,7 @@ export function mapDbCalendarToCalendarPost(p: DatabaseCalendar): CalendarPost {
     author: p.author,
     authorRole: p.author_role,
     tags: p.tags,
+    date: p.date,
     published: p.published,
     image: p.image || undefined,
   };
